28f5405946882d21cd0c9f2a51dae959bc85926d
michaelarayabe <michaelseifuaraya@gmail.com>
2020-11-08 23:24:15 +0100

    Using bootstrap and added the difference in infection perday

--- src/main/java/com/michael/coronavirustracker/HomeController.java
+++ src/main/java/com/michael/coronavirustracker/HomeController.java
@@ -1,19 +1,28 @@
 package com.michael.coronavirustracker;
 
+import com.michael.coronavirustracker.models.LocationStats;
 import com.michael.coronavirustracker.services.CoronaVirusDataService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
 public class HomeController {
 
     @Autowired
     CoronaVirusDataService coronaVirusDataService;
+
     @GetMapping("/")
     public String home(Model model){
-        model.addAttribute("locationStats",coronaVirusDataService.getAllStats());
+        List<LocationStats> allStats = coronaVirusDataService.getAllStats();
+        int totalReportedCases = allStats.stream().mapToInt(stat -> stat.getLatestTotalCases()).sum();
+        model.addAttribute("locationStats",allStats);
+        model.addAttribute("totalReportedCases",totalReportedCases);
+
+
         return "home";
     }
 }
